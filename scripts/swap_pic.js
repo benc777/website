b624b14b88f15b7473d47528009beb9adb7c3309
benc777 <80216843+benc777@users.noreply.github.com>
2025-02-03 12:32:18 -0500

    Initial commit from local folder

---
+++ scripts/swap_pic.js
@@ -0,0 +1,9 @@
+const profilePic = document.getElementById('profile-pic');
+
+profilePic.addEventListener('mouseenter', () => {
+  profilePic.src = 'assets/pepe_cook.png'; // Hover image
+});
+
+profilePic.addEventListener('mouseleave', () => {
+  profilePic.src = 'assets/ben_profile_pic.png'; // Original image
+});
